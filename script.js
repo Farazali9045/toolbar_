89296e5d42643d09424b7dee7b5aa3250b3adc61
Farazali9045 <97332711+Farazali9045@users.noreply.github.com>
2024-04-03 14:29:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let faClick = document.querySelector(".fa-grid-2-plus");
+let dropDown = document.querySelector(".dropDown");
+faClick.addEventListener('click',()=>{
+   dropDown.classList.toggle('changeDisplay');
+});
